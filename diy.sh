91e9a34cfb8c508c7e01ec9543106ad551abed02
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-03-23 00:32:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-src-git lienol https://github.com/Lienol/openwrt-package
+git clone https://github.com/Lienol/openwrt-package.git
 git clone https://github.com/tzxiaozhen88/koolproxyR.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git remote add -f origin https://github.com/vernesong/OpenClash.git
