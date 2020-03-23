31ff980bc944e01c27ea59933eb1aca9a0afa1a4
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-03-23 22:26:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/tzxiaozhen88/koolproxyR.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/Lienol/openwrt-package.git
+./scripts/feeds update -a
+./scripts/feeds install -a
