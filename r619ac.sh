307595aeacc7a81674ee5e55f5d797665fb334fd
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-03-25 21:57:31 +0800

    Create r619ac.sh

---
+++ r619ac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/tzxiaozhen88/koolproxyR.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone https://github.com/Lienol/openwrt-package.git
+git clone https://github.com/vernesong/OpenClash.git
