8fff3a2cd8262a9fb6731dfeb6caae27e39d4975
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-04-14 18:16:36 +0800

    Update r619ac.sh

--- r619ac.sh
+++ r619ac.sh
@@ -7,6 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
+git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
+git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon
+git clone https://github.com/pymumu/luci-app-smartdns                      package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns                       package/openwrt-smartdns
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
