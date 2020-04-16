bb44f86d9653c70eafd8f278996a2f9087b5ffaf
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-04-17 00:42:24 +0800

    Update r619ac_all.sh

--- r619ac_all.sh
+++ r619ac_all.sh
@@ -14,3 +14,5 @@ git clone https://github.com/tzxiaozhen88/koolproxyR.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/Lienol/openwrt-package.git
 git clone https://github.com/vernesong/OpenClash.git
+git clone https://github.com/zjwxh/lienol-openwrt-package.git
+git clone https://github.com/fw876/helloworld.git
