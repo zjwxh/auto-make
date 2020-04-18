f9b63a1c76a392d9257d1cd929253ca474716e87
zjwxh <61620117+zjwxh@users.noreply.github.com>
2020-04-18 10:46:12 +0800

    Update r619ac_all.sh

--- r619ac_all.sh
+++ r619ac_all.sh
@@ -7,12 +7,14 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/zjwxh/lienol-openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
 git clone https://github.com/tzxiaozhen88/koolproxyR.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/vernesong/OpenClash.git
-git clone https://github.com/fw876/helloworld.git
-git clone https://github.com/zjwxh/lienol-openwrt-package.git
+# git clone https://github.com/fw876/helloworld.git
+# git clone https://github.com/zjwxh/lienol-openwrt-package.git
 git clone https://github.com/Lienol/openwrt-package.git
